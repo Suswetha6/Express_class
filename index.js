8dc85ff23989bbd8b9d5f80cbcf74171b2845ed0
Suswetha <suswetha.23bcs10166@ms.sst.com>
2024-07-03 11:26:51 +0530

    CRUD

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express= require('express');
 const app = express();
+app.use(express.json());
+app.use(middleware);
+app.use(logger);
 
 let courses = [
   {id:1 , name:"java"},
@@ -10,6 +13,48 @@ let courses = [
 app.get('/courses',(req,res)=>{
   res.json(courses);
 })
+
+app.post('/courses', (req, res) => {
+  console.log(req.body);
+  let singleCourse = {
+    id:courses.length+1,
+    name:req.body.name
+  }
+  courses.push(singleCourse);
+  res.send(courses);
+});
+
+app.put('/courses/:id',(req,res)=>{
+  const courseId = parseInt(req.params.id,10); 
+  const course = courses.find(c => c.id === courseId);
+  course.name = req.body.name;
+  res.json(course);
+})
+
+app.delete('/courses/:id',(req,res)=>{
+  const courseId = parseInt(req.params.id,10); 
+  const course = courses.findIndex(c => c.id === courseId);
+  const deletedCourse = courses.splice(courseId, 1);
+  res.json(deletedCourse[0]);
+  res.json(course);
+})
+
+function middleware(req,res,next){
+  console.log("called");
+  const method = req.method;
+  const ip = req.ip;
+  const hostname = req.hostname;
+  const date = new Date().toISOString();
+
+  console.log(`[${date}] ${method} request from ${hostname} (IP: ${ip})`);
+  next();
+  
+}
+
+function logger(req,res,next){
+  console.log(req.method,req.ip,req.hostname,new  Data());
+  next();
+}
 
 app.listen(3000,()=>{
   console.log("server started");
